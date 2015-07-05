d55e56891e1a1c9789e2047455dc66c9bdb66960
mohawkeagle <themohawkeagle@gmail.com>
2015-07-05 19:53:03 -0300

    Implementation of the Sign up form

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "",
+                                email: "goo@gb",
+                                password: '12345',
+                                password_confirmation: '2145' }
+    end
+    assert_template 'users/new'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "teste",
+                                            email: "goo@gb.com",
+                                            password: '123455',
+                                            password_confirmation: '123455' }
+    end
+    assert_template 'users/show'
+  end
+end
