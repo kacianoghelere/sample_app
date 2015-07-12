5c8cc4ec8d0ef609ee10025fb43419f9d06cce26
mohawkeagle <themohawkeagle@gmail.com>
2015-07-12 06:32:18 -0300

    Madded some... fixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your e-mail to acctivate your account."
-      redirect_to root_url
+      # @user.send_activation_email
+      # flash[:info] = "Please check your e-mail to acctivate your account."
+      # redirect_to root_url
+      @user.activate
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
