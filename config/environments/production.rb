95c4fd8e37f55987ae5ec502ecba67de2d05a051
mohawkeagle <themohawkeagle@gmail.com>
2015-07-13 22:38:07 -0300

    Erros nos e-mails corrigidos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # https://whispering-mesa-6685.herokuapp.com/
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :production
+  
   host = 'https://whispering-mesa-6685.herokuapp.com/'
   config.action_mailer.default_url_options = {host: host}
   config.action_mailer.delivery_method = :smtp
